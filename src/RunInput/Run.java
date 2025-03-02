1cc02ddab73e249ae06143c0f7f3955fadabde1a
Yevhenii Usik <usik.evgen321@gmail.com>
2025-03-02 20:45:59 +0100

    lab1 gotowy

--- src/RunInput/Run.java
+++ src/RunInput/Run.java
@@ -1,7 +1,6 @@
 package RunInput;
 
 import Task.Tasks;
-import java.util.Scanner;
 
 public class Run {
 
@@ -17,14 +16,26 @@ public class Run {
         System.out.println("Zadanie 4");
         System.out.println("Zadanie 5");
         System.out.println("Zadanie 6");
-        System.out.println("Zadanie 8");
-        System.out.println("Wybierz zadananie:");
+        System.out.println("Zadanie 7");
+        System.out.println("Wybierz zadanie:");
 
         int wybor = input.InputInt();
         switch (wybor){
             case 1:
                 System.out.println(tasks.DataUser()); break;
             // kolejne case ....
+            case 2:
+                tasks.MathOperations(); break;
+            case 3:
+                System.out.println(tasks.parzNieparz()); break;
+            case 4:
+                System.out.println(tasks.podziel()); break;
+            case 5:
+                tasks.potega(); break;
+            case 6:
+                tasks.perwiastek(); break;
+            case 7:
+                System.out.println(tasks.trojkat()); break;
             default: System.out.println("Nie ma takiego zadania");
 
         }
--- src/Task/Tasks.java
+++ src/Task/Tasks.java
@@ -1,15 +1,62 @@
 package Task;
 import RunInput.Input;
-
 public class Tasks {
 
     Input input = new Input();
     public String DataUser(){
         return "Yevhenii Usik, 17(wkrótce 18)";
     }
-    public void MathOperations(int a, int b){
-        a = input.InputFloat();
-        b = input.InputFloat();
+    public void MathOperations(){
+        System.out.println("Input a b: ");
+        float a = input.InputFloat();
+        float b = input.InputFloat();
+        float sum = a+b;
+        float rozn = a-b;
+        float ilocz = a*b;
+        System.out.println("a + b = " + sum + ", a - b =" + rozn + ", a * b = " + ilocz);
     }
+    public boolean parzNieparz(){
+        System.out.println("Input a: ");
+        float a = input.InputFloat();
+        if (a % 2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean podziel(){
+        System.out.println("Input a: ");
+        float a = input.InputFloat();
+        if ((a % 3 == 0) && (a % 5 == 0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void potega(){
+        System.out.println("Input a: ");
+        float a = input.InputFloat();
+        System.out.println(a*a*a);
+    }
+    public void perwiastek(){
+        System.out.println("Input a: ");
+        float a = input.InputFloat();
+        double result = Math.sqrt(a);
+        System.out.println(result);
+    }
+    public boolean trojkat(){
+        System.out.println("Input a(min) b(max): ");
+        int a = input.InputInt();
+        int b = input.InputInt();
+        int n1 = a + (int)(Math.random() * ((b-a) + 1));
+        int n2 = a + (int)(Math.random() * ((b-a) + 1));
+        int n3 = a + (int)(Math.random() * ((b-a) + 1));
+        if ((n1*n1) + (n2*n2) == (n3*n3))
+            return true;
+        return false;
+    }
+
 
 }
