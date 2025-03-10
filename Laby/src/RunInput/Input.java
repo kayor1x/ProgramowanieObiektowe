4a968b0886fc0ce091139fd7bc0aa353e53aca0f
kayor1x <usik.evgen321@gmail.com>
2025-03-10 08:43:20 +0100

    dodane input string char boolean

--- Laby/src/RunInput/Input.java
+++ Laby/src/RunInput/Input.java
@@ -13,7 +13,12 @@ public class Input {
     public float InputFloat(){
         return scanner.nextFloat();
     }
+    //metoda do wczytywania typu String
+    public String InputString(){ return scanner.next(); }
 
+    public char InputChar(){ return scanner.next().charAt(0); }
+
+    public boolean InputBoolean(){ return scanner.nextBoolean(); }
 
 
 }
